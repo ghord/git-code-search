d17d63858142904fcb2aa73b59b739bcce1727f9
ghord <hordynski@outlook.com>
2020-12-01 17:03:55 +0100

    Initial commit

---
+++ src/GitCodeSearch/ViewModels/DialogHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitCodeSearch.ViewModels
+{
+    public static class DialogHelper
+    {
+        public static bool ShowDialog(UserControl view, string title, Window owner)
+        {
+            var dialogWindow = new Window();
+
+            dialogWindow.Owner = owner;
+            dialogWindow.Content = view;
+            dialogWindow.Title = title;
+            dialogWindow.Width = 800;
+            dialogWindow.Height = 600;
+            dialogWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            return dialogWindow.ShowDialog() == true;
+        }
+    }
+}
---
+++ src/GitCodeSearch/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GitCodeSearch.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodeSearch.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        public SettingsViewModel(Settings settings)
+        {
+            gitRepositories_ = settings.GitRepositores is null ? 
+                string.Empty :
+                string.Join(Environment.NewLine, settings.GitRepositores);
+        }
+
+        private string gitRepositories_;
+
+        public string GitRepositories
+        {
+            get { return gitRepositories_; }
+            set { SetField(ref gitRepositories_, value); }
+        }
+
+        public Settings GetSettings()
+        {
+            return new Settings
+            {
+                GitRepositores = gitRepositories_.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            };
+        }
+    }
+}
